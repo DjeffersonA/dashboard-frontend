2454632d3c7ee8a702d15634baefca195c3a9997
DjeffersonA <djefferson.sa@outlook>
2024-08-27 09:41:42 -0300

    add pages

---
+++ src/app/financeiro/page.tsx
@@ -0,0 +1,9 @@
+import AppContext from "@/context/app.context";
+
+export default function Financeiro() {
+  return (
+    <>
+      <AppContext>Dashboard Financeiro</AppContext>
+    </>
+  );
+}
---
+++ src/app/marketing/page.tsx
@@ -0,0 +1,9 @@
+import AppContext from "@/context/app.context";
+
+export default function Marketing() {
+  return (
+    <>
+      <AppContext>Dashboard Marketing</AppContext>
+    </>
+  );
+}
