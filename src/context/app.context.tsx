97f7e9716ba4510437b19a710c18e5c5edac4c11
DjeffersonA <djefferson.sa@outlook>
2024-10-02 08:58:09 -0300

    added logo to the loading page

--- src/context/app.context.tsx
+++ src/context/app.context.tsx
@@ -3,6 +3,7 @@ import SideNav from "@/components/sidenav.component";
 import { usePathname, useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useEffect, ReactNode } from "react";
+import Image from "next/image";
 
 export default function AppContext({ children }: { children: ReactNode }): JSX.Element {
   const pathName = usePathname();
@@ -17,10 +18,26 @@ export default function AppContext({ children }: { children: ReactNode }): JSX.E
 
   if (status === "loading") {
     return (
-      <div className="container bg-muted/40">
-        <div className="dot"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
+      <div className="bg-muted/40 relative">
+        <Image
+          src="/FGI_LIGHT_FULL.png"
+          alt="Logo Light"
+          width={258}
+          height={68}
+          className="absolute top-1/4 left-1/2 transform -translate-x-1/2 block dark:hidden"
+        />
+        <Image
+          src="/FGI_DARK_FULL.png"
+          alt="Logo Dark"
+          width={258}
+          height={68}
+          className="absolute top-1/4 left-1/2 transform -translate-x-1/2 hidden dark:block"
+        />
+        <div className="container relative pt-16">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
       </div>
     );
   }
