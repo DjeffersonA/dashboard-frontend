2aefc9e585a8a98887aea7d93f52e738104a5f11
Komorek <rafa.kaquino@gmail.com>
2024-11-22 20:06:50 -0300

    fix:bg loading and loagin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,110 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const { status, data: session } = useSession();
+  const [providers, setProviders] = useState<Providers>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (status === "loading") {
+    return (
+      <div className=" relative">
+        <Image
+          src="/FGI_LIGHT_FULL.png"
+          alt="Logo Light"
+          width={258}
+          height={68}
+          className="absolute top-1/4 left-1/2 transform -translate-x-1/2 block dark:hidden"
+        />
+        <Image
+          src="/FGI_DARK_FULL.png"
+          alt="Logo Dark"
+          width={258}
+          height={68}
+          className="absolute top-1/4 left-1/2 transform -translate-x-1/2 hidden dark:block"
+        />
+        <div className="container relative pt-16">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
         </div>
       </div>
+    );
+  }
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+  if (session && session.user && session.user.name) {
+    return (
+      <AppContext>
+        <div className="flex items-center">
+          <Image
+            src={session.user.image || '/default-profile.png'}
+            alt={`${session.user.name}'s profile`}
+            className="w-12 h-12 rounded-full mr-4"
+            width={12}
+            height={12}
+          />
+          <div className="flex flex-col">
+            <h1 className="text-lg font-semibold">
+              Bem-vindo, {(() => {
+                const nameParts = session.user.name.split(" ");
+                return `${nameParts[0]}`;
+              })()}!
+            </h1>
+            <p className="text-sm">Desenvolvedor de Software Jr.</p>
+            <p className="text-sm">{session.user.email}</p>
+          </div>
+        </div>
+      </AppContext>
+    );
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 ">
+      <div className="w-full max-w-md p-8 space-y-6 rounded-lg shadow-md bg-stone-100 dark:bg-muted/100">
+        <div className="w-full flex justify-center mt-6">
+          <Image
+            src="/FGI_LIGHT_FULL.png"
+            alt="Logo Light"
+            width={258}
+            height={68}
+            className="block dark:hidden"
+          />
+          <Image
+            src="/FGI_DARK_FULL.png"
+            alt="Logo Dark"
+            width={258}
+            height={68}
+            className="hidden dark:block"
+          />
+        </div>
+        <h1 className="text-2xl font-semibold text-center text-gray-900 dark:text-gray-100">
+          Dashboard
+        </h1>
+        <div className="space-y-4">
+          {providers && Object.values(providers).map((provider) => (
+            <div key={provider.name} className="flex justify-center">
+              <Button
+                className="w-full flex items-center justify-center space-x-2"
+                onClick={() => signIn(provider.id)}
+              >
+                <Image
+                  src="/google-icon.svg"
+                  alt="Google Logo"
+                  width={20}
+                  height={20}
+                />
+                <span>Continuar com Google</span>
+              </Button>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
